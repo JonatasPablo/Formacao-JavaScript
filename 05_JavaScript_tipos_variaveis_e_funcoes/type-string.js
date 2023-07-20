8d2d4d01f632ff423117deaafdd5dab77922111c
JonatasPablo <jonathaspabllo@gmail.com>
2023-07-19 22:35:10 -0300

    Add files via upload

---
+++ 05_JavaScript_tipos_variaveis_e_funcoes/type-string.js
@@ -0,0 +1,41 @@
+//const texto1 = "Olá, mundo!";
+//console.log(texto1);
+//const texto2 = 'Olá, mundo!';
+//console.log(texto2);
+//const senha = "senhaSuperSegura456!";
+//console.log(senha);
+//const StringDeNumeros = "34567";
+//console.log(StringDeNumeros);
+//const citacao = "Meu nome é ";
+//const meuNome = "Jonatas Pablo Candido da Silva";
+//console.log(citacao);
+
+// concetenação (+)
+//console.log(citacao + meuNome);
+
+// testar a transformação/conversão do código Unicode em caractere
+//const cifrao = '\u0024'
+//const aMaiusculo = '\u0041'
+//const tique = '\u2705'
+//const hiragana = '\u3041'
+//console.log(cifrao)
+//console.log(aMaiusculo)
+//console.log(tique)
+//console.log(hiragana)
+
+// Para fazer comparações entre strings
+//const cidade = "belo horizonte";
+//const input = "Belo Horizonte";
+//console.log(cidade === input);
+
+// Transformando todos os catacteres em letrar minúsculas
+//const cidade = "belo horizonte";
+//const input = "Belo Horizonte";
+//const inputMinusculo = input.toLowerCase();
+//console.log(cidade === inputMinusculo);
+
+// mostra quantos caracteres a string tem
+//const senha = "rafaelagoncalvesdasilva"
+//console.log(senha.length) // 13 caracteres
+
+// PESQUISAR =  template string OU template literal
