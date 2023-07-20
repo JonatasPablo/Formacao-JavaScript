4d803eea9dacac72a4b8deabacac6f1ae1c8d61e
JonatasPablo <jonathaspabllo@gmail.com>
2023-07-19 22:56:22 -0300

    modificação

--- conversoes.js
+++ conversoes.js
@@ -8,4 +8,53 @@ const numeroString = "456";
 // String()
 console.log(numero + Number(numeroString));
 
-// conversão explícita
+// conversão explícita String()
+//let telefone = 12341234;
+//console.log("O telefone é " + String(telefone)); // teremos a conversão do número 12341234 para uma string “12341234” e assim poderemos fazer a concatenação entre as strings
+
+// let telefone = 12341234;
+// console.log("O telefone é " + telefone.toString()); // o .toString() é uma outra forma para  fazer essa conversão, que é mais parecida com outras linguagens de programação.
+
+// let usuarioConectado = false;
+// console.log(String(usuarioConectado)); // teremos a conversão da booleana para string, nesse caso teremos uma string “false”.
+// usuarioConectado = true;
+// console.log(String(usuarioConectado)); // agora teremos uma string “true”.
+
+// conversão explícita Number()
+// Vamos calcular a área de um retângulo
+// let largura = "10";
+// let altura = "5";
+// console.log(Number(largura) * Number(altura)); // teremos a conversão de String para números, possibilitando a realização da multiplicação
+
+// let largura = "10";
+// let altura = "5";
+// console.log( + largura * + altura); // teremos a conversão de String para números realizado usando o + antes das variáveis
+
+// let meuNome = "leonardo";
+// console.log(Number(meuNome)); // como a variável meuNome não contém apenas números ele retorna o erro NaN (Not a Number, não é número);
+// console.log( + meuNome); // a conversão também retornará NaN
+
+// let usuarioConectado = false;
+// console.log(Number(usuarioConectado)); // teremos a conversão da booleana para número, sendo que false (falso) retorna o número 0.
+// usuarioConectado = true;
+// console.log(Number(usuarioConectado)); // agora teremos a conversão de true (verdadeiro) para o número 1.
+
+// {
+//     var respostaDeTudo = 3.14
+//     let idade = 42
+//     const pi = 29
+//     console.log(respostaDeTudo, idade, pi)
+
+// }
+// console.log(respostaDeTudo, idade, pi)
+
+let minhaLet;
+console.log(minhaLet); //undefined
+
+// atribuindo um valor com TIPO DE DADO STRING
+minhaLet = "eu sou um texto";
+console.log(minhaLet); // "eu sou um texto"
+
+// reatribuindo, desta vez com dado do TIPO NUMBER
+minhaLet = 100;
+console.log(minhaLet); //100
